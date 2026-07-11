bf2cbefda92aa40a80fa027241b6992f034a4fe1
Joyce Radis de Souza <255958806+joyceradis@users.noreply.github.com>
2026-07-11 19:19:28 -0300

    Create data.js

---
+++ assets/data.js
@@ -0,0 +1,21 @@
+window.ZERA_DATA = {
+  normalExam: {
+    estadoGeral: 'BEG, LOTE, CORADO, HIDRATADO, ANICTÉRICO, ACIANÓTICO, AFEBRIL',
+    acv: 'RCR, 2T, BNF, SEM SOPROS',
+    ar: 'MV PRESENTE BILATERALMENTE, SEM RUÍDOS ADVENTÍCIOS',
+    abd: 'PLANO, FLÁCIDO, INDOLOR À PALPAÇÃO, SEM SINAIS DE IRRITAÇÃO PERITONEAL',
+    ext: 'SEM EDEMAS, PULSOS PERIFÉRICOS PALPÁVEIS E SIMÉTRICOS',
+    neuro: 'LOTE, GLASGOW 15, SEM DÉFICITS NEUROLÓGICOS FOCAIS'
+  },
+  hppNegative: {
+    comorbidades: 'NEGA',
+    muc: 'NEGA',
+    alergias: 'NEGA',
+    habitos: 'NEGA',
+    cirurgias: 'NEGA'
+  },
+  storageKeys: {
+    drafts: 'zera-ps:drafts:v1',
+    autosave: 'zera-ps:autosave:v1'
+  }
+};
